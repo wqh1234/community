2b1b0d66ba7347d273f837a795176962406e1fa7
wqh1234 <250804486@qq.com>
2022-06-02 17:22:18 +0800

    第三次修改

--- src/main/java/com/example/demo02/HelloGitr.java
+++ src/main/java/com/example/demo02/HelloGitr.java
@@ -8,5 +8,6 @@ public class HelloGitr {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("1111111111111");
+        System.out.println("2222222222222");
     }
 }
