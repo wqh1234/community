e6d319d96a22d0ddb1a9dbaf8972110c4b519607
wqh1234 <250804486@qq.com>
2022-06-02 17:18:45 +0800

    首次提交

---
+++ src/main/java/com/example/demo02/HelloGitr.java
@@ -0,0 +1,11 @@
+package com.example.demo02;
+
+/**
+ * @author wqhstart
+ * @create 2022-06-02-17:16
+ */
+public class HelloGitr {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
